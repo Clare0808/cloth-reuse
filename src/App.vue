1c9ed700ee4c32bce7512be073302017955d1881
Clare0808 <yuhsin099@gmail.com>
2026-03-09 00:39:41 +0800

    modify home page and nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav v-if="route.path != '/'">
+    <div class="title-frame">
+      <img src="./assets/img/logo.png" />
+      <div class="title">舊衣回收平台</div>
+    </div>
+    <div class="func-text">
+      <router-link to="/">主頁</router-link>
+      <router-link to="/cloth">服飾專區</router-link>
+      <router-link to="/map">再生地圖</router-link>
+      <router-link to="/web-review">網站回饋</router-link>
+      <router-link to="/problem">問題回報</router-link>
+    </div>
+    <div class="func-icon">
+      <router-link to="/like">
+        <i class="fa-solid fa-heart"></i>
+      </router-link>
+      <router-link to="/user">
+        <i class="fa-solid fa-user"></i>
+      </router-link>
+    </div>
   </nav>
   <router-view />
 </template>
 
+<script>
+import { useRoute } from "vue-router";
+
+export default {
+  setup() {
+    const route = useRoute();
+
+    return {
+      route,
+    };
+  },
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Pacifico", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+}
+body {
+  margin: 0;
+  padding: 0;
 }
 
 nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: #e7e9e6;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav a {
+  color: #3b5131;
+  font-size: 22px;
+  text-decoration: none;
+  margin: 20px;
+}
+.title-frame {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title-frame img {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+}
+.title {
+  color: #3b5131;
+  font-size: 28px;
+}
+.func-icon a {
+  font-size: 20px;
+  margin: 10px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="page">
+    <div class="title-frame">
+      <div class="title">舊衣回收平台</div>
+      <div class="slogan">讓每件衣服再走一段路</div>
+      <div class="btn">開始</div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style scoped>
+.page {
+  height: 100vh;
 
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+  background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url("@/assets/img/background3.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+
+  overflow-y: hidden;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.title-frame {
+  color: #3b5131;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  font-size: 56px;
+}
+.slogan {
+  color: #3b5131;
+  font-size: 26px;
+  margin-top: 20px;
+}
+.btn {
+  font-size: 22px;
+  background-color: #bcbfb1;
+  border-radius: 12px;
+  margin-top: 30px;
+  padding: 10px 20px;
+  transition: all 0.3s ease;
+}
+.btn:hover {
+  background-color: #d1d4c1;
+  cursor: pointer;
+  transform: scale(1.2);
+}
+</style>
